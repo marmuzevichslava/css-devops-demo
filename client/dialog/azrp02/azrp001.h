57bdb886e870b5c6fa6b140a8de7e372fa924a93
cwoods <cwoods@us.nationalgrid.com>
1995-10-24 09:33:24 -0400

    Initial revision.

---
+++ client/dialog/azrp02/azrp001.h
@@ -0,0 +1,165 @@
+/***************************************************************************
+**
+**               Customer Service System Application Header File
+**
+**  FILENAME         : <CUffnnn>.H
+**
+**  DESCRIPTION      : Window Header File
+**
+**  AUTHOR           : XXXXXXXX
+**
+**  DATE CREATED     : 99/99/99
+**
+**  REVISION HISTORY :
+**
+**    DATE      REVISED BY   SIR #    DESCRIPTION OF CHANGE
+**    --------  -----------  -------  -------------------------------------
+**    99/99/99  XXXXXXXX              Original code.
+**
+***************************************************************************/
+
+/***************************************************************************/
+/* Application #includes                                                   */
+/***************************************************************************/
+
+//---------------------------------------------------
+//  S H E L L   I N S T R U C T I O N S
+//
+//  If a dialogname.h file exists for the dialog that
+//  contains this window, and this window requires
+//  access to that header file, then uncomment the
+//  line below.  Otherwise, delete it.
+//
+//  REMOVE THIS COMMENT BLOCK WHEN DONE.
+//
+//---------------------------------------------------
+//#include CUffnn.H
+
+/***************************************************************************/
+/* Application #defines                                                    */
+/***************************************************************************/
+
+/*             FMT    LEN   PREC  US    OCCUR BYTES OTHER */
+#define SEG_RPT_DTL_FMT \
+        "      %-2.2s %3.3s %4.4s %2.2s %5.5s %5.5s %-22.22s\n"
+
+#define SEG_RPT_COLHDR1 \
+"      -----------------------------------------------------------------------------------------------------------------------------"
+
+#define SEG_RPT_COLHDR2 \
+"      DOCUMENT ID              ITEM TYPE      ITEM NAME                          FMT LEN PREC US OCCUR BYTES OTHER CHARACTERISTICS "
+
+#define SEG_RPT_COLHDR3 \
+"      ---------------------    ----------     ------------------------------     --- --- ---- -- ----- ----- ----------------------"
+
+/***************************************************************************/
+/* Application typedefs                                                    */
+/***************************************************************************/
+
+
+/***************************************************************************/
+/* Forward declarations for Application Validation Functions               */
+/***************************************************************************/
+
+//---------------------------------------------------
+//  S H E L L   I N S T R U C T I O N S
+//
+//  Uncomment and duplicate the following line for every
+//  RAB-generated Application Validation function that has
+//  been added to the <CUffnnn>.VLD file.
+//
+//  REMOVE THIS COMMENT BLOCK WHEN DONE.
+//
+//---------------------------------------------------
+
+WCBFWD( AZRP001BusAmnprintClk )
+WCBFWD( AZRP001BusFindpbClk )
+WCBFWD( AZRP001BusAmnbatchClk )
+
+SHORT SegRptRetrieveLayout( _REQUESTHDR *pRequestHdr,
+                            _REPLYHDR *pReplyHdr,
+                            _ENTITYDATA *pEntityDataTable,
+                            CMN_ARCH_PARM_TYPES );
+
+SHORT AZRP001PopulateWindow( _ENTITYDATA *pEntityDataTable,
+                             _REPLYHDR *pReplyHdr,
+                             CMN_ARCH_PARM_TYPES );
+
+SHORT PrintHeader( FILE *pfPrinter, _REQUESTHDR *pRequestHdr,
+                   _REPLYHDR *pReplyHdr, _ENTITYDATA *pEntityDataTable,
+                   SHORT CurrPage, SHORT *pCurrLine );
+
+SHORT PrintSegmentReport( _REQUESTHDR *pRequestHdr, _REPLYHDR *pReplyHdr,
+                          _ENTITYDATA *pEntityDataTable, CHAR *PrintFile,
+                          CHAR Duplex, CMN_ARCH_PARM_TYPES );
+
+//---------------------------------------------------
+//  S H E L L   I N S T R U C T I O N S
+//
+//  Uncomment and duplicate the following statement for every
+//  application-defined Application Validation function that
+//  has been added to the <CUffnnn>.VLD file.
+//
+//  REMOVE THIS COMMENT BLOCK WHEN DONE.
+//
+//---------------------------------------------------
+
+//SHORT CUffnnnVldApplicationDefined( TYPE Parameter1,
+//                                    TYPE Parameter2,
+//                                    TYPE ParameterN,
+//                                    CMN_ARCH_PARM_TYPES);
+
+
+/***************************************************************************/
+/* Forward declarations for Application Business Functions                 */
+/***************************************************************************/
+
+//---------------------------------------------------
+//  S H E L L   I N S T R U C T I O N S
+//
+//  Uncomment and duplicate the following line for every
+//  RAB-generated Application Business function that has
+//  been added to the <CUffnnn>.BUS file.
+//
+//  REMOVE THIS COMMENT BLOCK WHEN DONE.
+//
+//---------------------------------------------------
+
+//WCBFWD( CUffnnnBusControlEvent )
+
+//---------------------------------------------------
+//  S H E L L   I N S T R U C T I O N S
+//
+//  Uncomment and duplicate the following statement for every
+//  application-defined Application Business function that
+//  has been added to the <CUffnnn>.BUS file.
+//
+//  REMOVE THIS COMMENT BLOCK WHEN DONE.
+//
+//---------------------------------------------------
+
+//SHORT CUffnnnBusApplicationDefined( TYPE Parameter1,
+//                                    TYPE Parameter2,
+//                                    TYPE ParameterN,
+//                                    CMN_ARCH_PARM_TYPES);
+
+
+/***************************************************************************/
+/* Forward declarations for Architecture Exit Functions                    */
+/***************************************************************************/
+
+//---------------------------------------------------
+//  S H E L L   I N S T R U C T I O N S
+//
+//  Uncomment and duplicate the following statement for every
+//  application-defined Architecture Exit function that
+//  has been added to the <CUffnnn>.AEX file.
+//
+//  REMOVE THIS COMMENT BLOCK WHEN DONE.
+//
+//---------------------------------------------------
+
+//SHORT CUffnnnAexApplicationDefined( TYPE Parameter1,
+//                                    TYPE Parameter2,
+//                                    TYPE ParameterN,
+//                                    CMN_ARCH_PARM_TYPES);
---
+++ client/dialog/azrp02/azrp003.h
@@ -0,0 +1,135 @@
+/***************************************************************************
+**
+**               Customer Service System Application Header File
+**
+**  FILENAME         : <CUffnnn>.H
+**
+**  DESCRIPTION      : Window Header File
+**
+**  AUTHOR           : XXXXXXXX
+**
+**  DATE CREATED     : 99/99/99
+**
+**  REVISION HISTORY :
+**
+**    DATE      REVISED BY   SIR #    DESCRIPTION OF CHANGE
+**    --------  -----------  -------  -------------------------------------
+**    99/99/99  XXXXXXXX              Original code.
+**
+***************************************************************************/
+
+/***************************************************************************/
+/* Application #includes                                                   */
+/***************************************************************************/
+
+//---------------------------------------------------
+//  S H E L L   I N S T R U C T I O N S
+//
+//  If a dialogname.h file exists for the dialog that
+//  contains this window, and this window requires
+//  access to that header file, then uncomment the
+//  line below.  Otherwise, delete it.
+//
+//  REMOVE THIS COMMENT BLOCK WHEN DONE.
+//
+//---------------------------------------------------
+//#include CUffnn.H
+
+/***************************************************************************/
+/* Application #defines                                                    */
+/***************************************************************************/
+
+
+/***************************************************************************/
+/* Application typedefs                                                    */
+/***************************************************************************/
+
+
+/***************************************************************************/
+/* Forward declarations for Application Validation Functions               */
+/***************************************************************************/
+
+//---------------------------------------------------
+//  S H E L L   I N S T R U C T I O N S
+//
+//  Uncomment and duplicate the following line for every
+//  RAB-generated Application Validation function that has
+//  been added to the <CUffnnn>.VLD file.
+//
+//  REMOVE THIS COMMENT BLOCK WHEN DONE.
+//
+//---------------------------------------------------
+
+//WCBFWD( CUffnnnVldControlEvent )
+
+//---------------------------------------------------
+//  S H E L L   I N S T R U C T I O N S
+//
+//  Uncomment and duplicate the following statement for every
+//  application-defined Application Validation function that
+//  has been added to the <CUffnnn>.VLD file.
+//
+//  REMOVE THIS COMMENT BLOCK WHEN DONE.
+//
+//---------------------------------------------------
+
+//SHORT CUffnnnVldApplicationDefined( TYPE Parameter1,
+//                                    TYPE Parameter2,
+//                                    TYPE ParameterN,
+//                                    CMN_ARCH_PARM_TYPES);
+
+
+/***************************************************************************/
+/* Forward declarations for Application Business Functions                 */
+/***************************************************************************/
+
+//---------------------------------------------------
+//  S H E L L   I N S T R U C T I O N S
+//
+//  Uncomment and duplicate the following line for every
+//  RAB-generated Application Business function that has
+//  been added to the <CUffnnn>.BUS file.
+//
+//  REMOVE THIS COMMENT BLOCK WHEN DONE.
+//
+//---------------------------------------------------
+
+WCBFWD( AZRP003BusOkpbClk )
+WCBFWD( AZRP003BusPrdis   )
+
+//---------------------------------------------------
+//  S H E L L   I N S T R U C T I O N S
+//
+//  Uncomment and duplicate the following statement for every
+//  application-defined Application Business function that
+//  has been added to the <CUffnnn>.BUS file.
+//
+//  REMOVE THIS COMMENT BLOCK WHEN DONE.
+//
+//---------------------------------------------------
+
+//SHORT CUffnnnBusApplicationDefined( TYPE Parameter1,
+//                                    TYPE Parameter2,
+//                                    TYPE ParameterN,
+//                                    CMN_ARCH_PARM_TYPES);
+
+
+/***************************************************************************/
+/* Forward declarations for Architecture Exit Functions                    */
+/***************************************************************************/
+
+//---------------------------------------------------
+//  S H E L L   I N S T R U C T I O N S
+//
+//  Uncomment and duplicate the following statement for every
+//  application-defined Architecture Exit function that
+//  has been added to the <CUffnnn>.AEX file.
+//
+//  REMOVE THIS COMMENT BLOCK WHEN DONE.
+//
+//---------------------------------------------------
+
+//SHORT CUffnnnAexApplicationDefined( TYPE Parameter1,
+//                                    TYPE Parameter2,
+//                                    TYPE ParameterN,
+//                                    CMN_ARCH_PARM_TYPES);
