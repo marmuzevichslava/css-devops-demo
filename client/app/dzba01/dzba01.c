f75122fe5d43ff19011fcf96ca4f9255e942487f
mconner <mconner@us.nationalgrid.com>
1996-08-30 14:13:26 -0400

    This is version 1.0 to correct an overwrite from the host side pvcs checkin

--- client/app/dzba01/dzba01.c
+++ client/app/dzba01/dzba01.c
@@ -2,43 +2,42 @@
  *                                                                         *
  *                     P R O G R A M   F R O N T   E N D                   *
  *                                                                         *
- *                  Copyright (C) 1994 Andersen Consulting.                *
- *                         All rights reserved.                            *
+ *   (c) Copyright 1994,1996  Andersen Consulting.  All Rights Reserved.   *
  *                                                                         *
  ***************************************************************************
  *                                                                         *
- *                Program Front End for: dzba01                            *
- *                         Generated on: Sun Jul  7 15:23:39 1996          *
+ *                Program Front End for: DZBA01                            *
+ *                         Generated on: Thu Aug 29 17:59:08 1996          *
  *                                   by: IPEREZAR                          *
+ *                    Short Description:                                   *
  *                                                                         *
  ***************************************************************************/
 
-#define  INCL_WIN
-
-#define  FND_VERSION2
 #define  FND_FE_INCL
 #define  FND_IM_INCL
 #define  FND_EH_INCL
 #define  FND_PS_INCL
+#define  FND_CF_INCL
 
 /***************************************************************************
  * Foundation Application Information                                      *
  ***************************************************************************/
-#define  FND_PROGRAM_NAME        "dzba01"
+#define  FND_PROGRAM_NAME        "DZBA01"
 #define  FND_APPL_VERSION        "21"
-#define  FND_APPL_QUEUE          UNSOLICITED_Q_AU
-#define  FND_PROGRAM_TYPE        FND_APPL_TYPE_SERVER
-#define  FND_PROGRAM_LANG        FND_COBOL_LANG
-#define  FND_DBMS                FND_DB_ORACLE
-#define  FND_PORTABILITY_FLAG    0
+#define  FND_APPL_QUEUE          UNSOLICITED_Q_AW
+#define  FND_PROGRAM_TYPE        FND_APPL_TYPE_CLIENT
+#define  FND_PROGRAM_LANG        FND_C_LANG
+#define  FND_DBMS                FND_DB_NONE
+#define  FND_PORTABILITY_FLAG    TRUE
+#define  FND_WINDOW_NAME         ""
 
 /***************************************************************************
  * Register with Foundation services                                       *
  ***************************************************************************/
-#define REGISTER_WITH_MM         1
-#define REGISTER_WITH_EL         0
-#define REGISTER_WITH_ST         1
-#define REGISTER_WITH_MN         1
+#define REGISTER_WITH_MM         FALSE
+#define REGISTER_WITH_EL         FALSE
+#define REGISTER_WITH_ST         TRUE
+#define REGISTER_WITH_MN         TRUE
 
 /***************************************************************************
  * User Supplied Definitions.                                              *
@@ -47,13 +46,14 @@
 /***************************************************************************
  * System Headers                                                          *
  ***************************************************************************/
+#include <windows.h>
+#include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 /*************************************************************************** 
  * Foundation Global Header File                                           *
  ***************************************************************************/
-#include <kglhk000.h>
+#include <kglxk000.h>
 
 /***************************************************************************
  * Business Function Context Data declaration.                             *
@@ -62,26 +62,19 @@
 #define _BFCD            void
 #define FND_BFCD_SIZE    64000      /* Cobol default for BFCD */
 #else
-#define _BFCD            void
-#define FND_BFCD_SIZE    0
+#include "dzba01b.gnb"
+
 #endif
 
 /***************************************************************************
  *  Service, Initialization and Termination Function Prototypes            *
  ***************************************************************************/
-extern FND_FUNCTION DZBA001;
 
-
-
-/***************************************************************************
- * Window definition tables                                                *
- ***************************************************************************/
-#include "dzba01.wdt"
 
 /***************************************************************************
  * Service dispatch table                                                  *
  ***************************************************************************/
-#include "dzba01.sdt"
+#include "DZBA01.sdt"
 
 /***************************************************************************
  * User Includes.                                                          *
@@ -89,16 +82,22 @@ extern FND_FUNCTION DZBA001;
 
 
 
-int main(int argc, char *argv[])
+int WINAPI WinMain (HINSTANCE hInstance,      /* current instance handle        */
+                    HINSTANCE hPrevInstance,  /* previous instance handle       */
+                    LPSTR     lpCmdLine,      /* command line                   */
+                    int       nCmdShow )      /* show window type (open or icon)*/
 {
+/***************************************************************************
+ * Window definition tables                                                *
+ ***************************************************************************/
+#include "DZBA01.wdt"
 
 /***************************************************************************
  * Application Interface Initialization                                    *
  ***************************************************************************/
+
    APPL_INIT_DATA  applInitData;
-   unsigned short  returnStatus;
-   
-   printf("ENTERED BAEXT FRONT END.\n");
+   unsigned short  returnStatus = FND_SUCCESS;
 
    memcpy(applInitData.version,            FND_APPL_VERSION, _VER_LEN);
    memcpy(applInitData.programName,        FND_PROGRAM_NAME, _PROG_NAME_LEN);
@@ -110,8 +109,8 @@ int main(int argc, char *argv[])
    applInitData.initFlags.registerWithMN = REGISTER_WITH_MN;
    applInitData.initRoutine              = FND_PROGRAM_INIT_ROUTINE;
    applInitData.termRoutine              = FND_PROGRAM_TERM_ROUTINE;
-   applInitData.initUIRoutine            = FND_INTERFACE_INIT_ROUTINE;
-   applInitData.termUIRoutine            = FND_INTERFACE_TERM_ROUTINE;
+   applInitData.initUIRoutine            = (SERVICE_FUNC_PTR) FND_INTERFACE_INIT_ROUTINE;
+   applInitData.termUIRoutine            = (SERVICE_FUNC_PTR) FND_INTERFACE_TERM_ROUTINE;
    applInitData.applDispatchTable        = dispatchTable;
    applInitData.numApplMessages = sizeof(dispatchTable)/sizeof(_FE_MSG_DISPATCH_RECORD);
    applInitData.pBFCD                    = NULL;
@@ -127,10 +126,15 @@ int main(int argc, char *argv[])
    applInitData.DynamicAppl              = NULL;
    applInitData.DynamicFree              = NULL;
    applInitData.DynamicSrv               = 0;
+   applInitData.hInstance                = hInstance;
+   applInitData.hPrevInstance            = hPrevInstance;
+   applInitData.pszCmdLine               = lpCmdLine;
+   applInitData.nCmdShow                 = nCmdShow;
+   applInitData.ThreadStackSize          = 32768;
 
-   printf("Calling Application %s.\n",argv[0]);
 
-   FND_Application(argc,argv,&applInitData,returnStatus);
+
+   FND_Application(__argc,__argv,&applInitData,returnStatus);
 
    return 0;
 }
