bbf10b5a45f0e8b6d46c49827b2650e8f0efdb8c
iperezar <iperezar@us.nationalgrid.com>
1997-02-19 16:13:30 -0500

    The module pertains to a new window in the Bill Account Extract dialog us

---
+++ client/app/dzba01/dzba004i.h
@@ -0,0 +1,53 @@
+/***************************************************************************
+*                                                                          *
+*                      C    H E A D E R    F I L E                         *
+*                                                                          *
+*    (c) Copyright 1994,1996  Andersen Consulting.  All Rights Reserved.   *
+*                                                                          *
+****************************************************************************
+*                                                                          *
+*                     Header file for: DZBA004I                            *
+*                        Generated on: Thu Jan 30 09:46:52 1997            *
+*                                  by: IPEREZAR                            *
+*                   Short Description:                                     *
+*                                                                          *
+****************************************************************************/
+
+/***************************************************************************
+* Definition for Record Group.DZBA004I
+***************************************************************************/
+#ifndef   DEV00860_EXPORT_FILE_LEN
+#define   DEV00860_EXPORT_FILE_LEN              9
+#define   EXPORTFILE_LEN                        9
+#endif
+#ifndef   DEV00861_IMPORT_FILE_LEN
+#define   DEV00861_IMPORT_FILE_LEN              9
+#define   IMPORTFILE_LEN                        9
+#endif
+#ifndef   DEV00862_WORKING_DIRECTORY_LEN
+#define   DEV00862_WORKING_DIRECTORY_LEN        9
+#define   WORKINGDIRECTORY_LEN                  9
+#endif
+#ifndef _DZBA004INPUT_z
+#define _DZBA004INPUT_z
+
+typedef struct __DZBA004Input
+{
+   char                  ExportFile[9];
+   char                  ImportFile[9];
+   char                  WorkingDirectory[9];
+}  _DZBA004INPUT;
+#endif
+/***************************************************************************
+* Definition for Record.DZBA004I
+***************************************************************************/
+#ifndef _DZBA004INPUTREC_z
+#define _DZBA004INPUTREC_z
+
+typedef struct __DZBA004InputRec
+{
+   _DZBA004INPUT         DZBA004Input;
+}  _DZBA004INPUTREC;
+#endif
+
+
---
+++ client/app/dzba01/dzba004x.c
@@ -0,0 +1,137 @@
+/***************************************************************************
+ *                                                                         *
+ *            C  W I N D O W S   N T   W I N D O W   F I L E               *
+ *                                                                         *
+ *   (c) Copyright 1994,1996  Andersen Consulting.  All Rights Reserved.   *
+ *                                                                         *
+ ***************************************************************************
+ *                                                                         *
+ *                      Module File for: DZBA004X DETAILS FOR IMP/EXP      *
+ *                         Generated on: Thu Jan 30 09:46:52 1997          *
+ *                                   by: IPEREZAR                          *
+ *                    Short Description:                                   *
+ *                                                                         *
+ ***************************************************************************
+ * You may need to define other constants depending on the APIs used       *
+ * in your window.                                                         *
+ ***************************************************************************/
+
+#define CMN_ERR_ARCH_WRAP_FUNC FALSE
+#define WINDOWNAME DZBA004
+
+
+#define INCL_DOS
+#define INCL_WIN
+#define WINDOWMOD
+
+/***************************************************************************
+ * FOUNDATION Data Structures And Common Functions                         *
+ ***************************************************************************/
+#define  FND_CF_INCL
+#define  FND_CTCONV_INCL
+#define  FND_EH_INCL
+#define  FND_IM_INCL
+#define  FND_PS_INCL
+#define  FND_SD_INCL
+#define  FND_ST_INCL
+#define  FND_MS_INCL
+
+#define WESMAPNAME _DZBA004WesMap
+
+/***************************************************************************
+ * User Supplied Definitions.                                              *
+ ***************************************************************************/
+
+/***************************************************************************
+ * System Header Files                                                     *
+ ***************************************************************************/
+#include <windows.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <math.h>
+#include <limits.h>
+#include <float.h>
+
+/*************************************************************************** 
+ * Foundation Global Header File                                           *
+ ***************************************************************************/
+#include <kglxk000.h>
+
+/***************************************************************************
+ * Business Function Context Data Type Definition                          *
+ ***************************************************************************/
+#include "dzba01b.gnb"
+
+
+/***************************************************************************
+ * CSS Architecture Global Header File                                     *
+ ***************************************************************************/
+#include <C1CEAB.H>
+
+/***************************************************************************
+ * Window Context Data
+ ***************************************************************************/
+#include "dzba004c.h"
+
+#if !defined(WINCONTEXTNAME)
+#define WINCONTEXTNAME       void
+#define FND_WINCONTEXT_LEN   0
+#else
+#define FND_WINCONTEXT_LEN   sizeof(WINCONTEXTNAME)
+#endif
+
+/***************************************************************************
+ * WESMap Header File                                                      *
+ ***************************************************************************/
+#include "dzba004.wmp"
+
+
+/*************************************************************************** 
+ * Application Window Identifiers, window control identifiers and the      *
+ * window dispatch Tables.                                                 *
+ ***************************************************************************/
+#include "DZBA01.gnz"
+#include "dzba004.gnh"
+#include "dzba004.gnd"
+
+/***************************************************************************
+ * CSS Architecture Global Header File                                     *
+ ***************************************************************************/
+#define INCL_C1CBASE
+#define INCL_C1CCALLBACK
+#include <C1C.H>
+
+/***************************************************************************
+ * CSS Functional Header File                                              *
+ ***************************************************************************/
+#include <CSS.H>
+
+
+/***************************************************************************
+ * User Includes.                                                          *
+ ***************************************************************************/
+
+/***************************************************************************
+ * Copybooks and Source Includes
+ ***************************************************************************/
+#include "dzba004i.h"
+#include "dzba004o.h"
+
+
+/***************************************************************************
+ * Window Header File                                                      *
+ ***************************************************************************/
+#include "dzba004.h"
+
+/***************************************************************************
+ * Callbacks                                                               *
+ ***************************************************************************/
+#include "dzba004.aex"
+// #include "archhelp.cb"
+#include "azdi0400.c"
+#include "dzba004.bus"
+
+
+#include "dzba004.src"
